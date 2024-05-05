7d92c176c989eeb9c60a20ed242c2856beabd651
Innocent WK Chinyemba <innocentwkc@gmail.com>
2024-05-05 14:59:50 +0200

    add: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  server: {
+    port: 4080, // Use port 5000 for the web app frontend
+  },
+  build: {
+    outDir: "dist", // Specify the output directory
+  },
+  plugins: [vue(), VueDevTools()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
